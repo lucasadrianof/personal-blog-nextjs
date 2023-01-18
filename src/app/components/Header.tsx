dcb74acb710cce4829ec40bbfa408f25c74f31af
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-01-18 20:14:34 -0300

    Add GitHub action to validate Linters/TypeScript (#3)
    * chore: add TS eslint rules
    
    * chore: add prettier
    
    * style: prettier fixes
    
    * chore: add "ci" script
    
    * chore: remove CI script
    
    * feat: add GH to validate linters and TSC

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,7 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Image from "next/image";
-import Link from "next/link";
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Header() {
   return (
@@ -13,10 +13,18 @@ export default function Header() {
       </div>
       <nav>
         <ul className="pr-6 flex items-center text-white">
-          <li className="pl-6"><Link href="/about">About me</Link></li>
-          <li className="pl-6"><Link href="/about">Resume</Link></li>
-          <li className="pl-6"><Link href="/blog">Blog</Link></li>
-          <li className="pl-6"><Link href="/blog">Contact</Link></li>
+          <li className="pl-6">
+            <Link href="/about">About me</Link>
+          </li>
+          <li className="pl-6">
+            <Link href="/about">Resume</Link>
+          </li>
+          <li className="pl-6">
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li className="pl-6">
+            <Link href="/blog">Contact</Link>
+          </li>
         </ul>
       </nav>
       <div className="block lg:hidden">
@@ -24,6 +32,6 @@ export default function Header() {
           <FontAwesomeIcon className="text-4xl text-black" icon={faBars} />
         </button>
       </div>
-    </div >
+    </div>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import type { AppProps } from 'next/app'
 
-import { config } from "@fortawesome/fontawesome-svg-core"
-import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
-config.autoAddCss = false;
+config.autoAddCss = false
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Component {...pageProps} />
-  )
+  return <Component {...pageProps} />
 }
