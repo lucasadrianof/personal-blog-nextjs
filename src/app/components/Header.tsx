aafdb0267c46db1e0dab2ab5221036d70960e660
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-01-18 19:46:50 -0300

    Navbar updates (#2)
    * chore: add app logo
    
    * chore: center tailwind container
    
    * feat: basic nav header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,24 +1,29 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 import Link from "next/link";
 
-const Header = () => (
-  <nav className="container flex items-center justify-between">
-    <div>
-      <Link className="w-24 lg:w-48" href="/">Home</Link>
-    </div>
-    <div className="hidden lg:block">
-      <ul className="flex items-center">
-        <li className="pl-6"><Link href="/blog">Blog</Link></li>
-        <li className="pl-6"><Link href="/about">About me</Link></li>
-      </ul>
-    </div>
-    <div className="block lg:hidden">
-      <button className="w-5">
-        <FontAwesomeIcon className="text-4xl text-black" icon={faBars} />
-      </button>
-    </div>
-  </nav>
-)
-
-export default Header
+export default function Header() {
+  return (
+    <div className="container w-full h-24 bg-black flex items-center place-content-between">
+      <div className="pl-6">
+        <Link href="/">
+          <Image alt="logo" src="/logo.png" width={190} height={30} />
+        </Link>
+      </div>
+      <nav>
+        <ul className="pr-6 flex items-center text-white">
+          <li className="pl-6"><Link href="/about">About me</Link></li>
+          <li className="pl-6"><Link href="/about">Resume</Link></li>
+          <li className="pl-6"><Link href="/blog">Blog</Link></li>
+          <li className="pl-6"><Link href="/blog">Contact</Link></li>
+        </ul>
+      </nav>
+      <div className="block lg:hidden">
+        <button className="w-5">
+          <FontAwesomeIcon className="text-4xl text-black" icon={faBars} />
+        </button>
+      </div>
+    </div >
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from './components/Header'
 import './globals.css'
 
 export default function RootLayout({
@@ -8,7 +9,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className="overflow-hidden max-h-screen">
+      <body className="overflow-hidden max-h-screen w-full">
+        <Header />
         {children}
       </body>
     </html>
