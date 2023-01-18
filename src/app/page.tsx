56f2c06b134013f09e76ba461c975be6c90d00e1
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-01-18 20:05:15 -0300

    style: prettier fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div id="main">
-      <div>
-        123
-      </div>
+      <div>123</div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}"
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       container: {
-        center: true
-      }
+        center: true,
+      },
     },
   },
   plugins: [],
