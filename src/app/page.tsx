2fd01d22c8907bbbb0f8cc54643b0c5b4835099e
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-01-18 19:44:52 -0300

    feat: basic nav header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Header from "./components/Header"
-
 export default function Home() {
   return (
     <div id="main">
-      <div className="flex justify-center w-full top-0 py-3 sm:py-5 absolute">
-        <Header />
+      <div>
+        123
       </div>
     </div>
   )
