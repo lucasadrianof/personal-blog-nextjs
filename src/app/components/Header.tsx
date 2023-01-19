0ab5437bf201319ef3180145231d69635ddd26ff
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-01-18 21:56:28 -0300

    feat: hover effect on menu links

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,5 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
-import { faAddressCard, faFile, faPaperPlane } from '@fortawesome/free-regular-svg-icons'
+import { faCircleUser, faFile, faPaperPlane } from '@fortawesome/free-regular-svg-icons'
 import { faBars, faHouse, faRss } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
@@ -13,7 +13,7 @@ type MenuLinkProps = {
 
 const MenuLink = ({ icon, href, text }: MenuLinkProps) => (
   <li className="pl-6">
-    <Link className="pl-6" href={href} >
+    <Link className="px-5 py-3 rounded-md font-medium text-sm bg-[#212425] text-[#A6A6A6] hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476]" href={href} >
       <span className="mr-2 text-xl">
         <FontAwesomeIcon icon={icon} />
       </span>
@@ -24,7 +24,7 @@ const MenuLink = ({ icon, href, text }: MenuLinkProps) => (
 
 export default function Header() {
   return (
-    <div className="container w-full h-24 bg-black flex items-center place-content-between">
+    <div className="container w-full h-24 flex items-center place-content-between">
       <div className="pl-6">
         <Link href="/">
           <Image alt="logo" src="/logo.png" width={190} height={30} />
@@ -32,7 +32,8 @@ export default function Header() {
       </div>
       <nav>
         <ul className="pr-6 flex items-center text-white">
-          <MenuLink icon={faHouse} href="/about" text="About me" />
+          <MenuLink icon={faHouse} href="/" text="Home" />
+          <MenuLink icon={faCircleUser} href="/about" text="About me" />
           <MenuLink icon={faFile} href="/resume" text="Resume" />
           <MenuLink icon={faRss} href="/blog" text="Blog" />
           <MenuLink icon={faPaperPlane} href="/contact" text="Contact" />
