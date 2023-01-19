3b972ec17f905d81d174ae646e69701d9f1e2d07
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-01-18 22:07:30 -0300

    style: lint fixing

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,9 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
-import { faCircleUser, faFile, faPaperPlane } from '@fortawesome/free-regular-svg-icons'
+import {
+  faCircleUser,
+  faFile,
+  faPaperPlane,
+} from '@fortawesome/free-regular-svg-icons'
 import { faBars, faHouse, faRss } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
@@ -13,12 +17,15 @@ type MenuLinkProps = {
 
 const MenuLink = ({ icon, href, text }: MenuLinkProps) => (
   <li className="pl-6">
-    <Link className="px-5 py-3 rounded-md font-medium text-sm bg-[#212425] text-[#A6A6A6] hover:text-white hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476]" href={href} >
+    <Link
+      className="px-5 py-3 rounded-md font-medium text-sm bg-[#212425] text-[#A6A6A6] hover:text-white hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476]"
+      href={href}
+    >
       <span className="mr-2 text-xl">
         <FontAwesomeIcon icon={icon} />
       </span>
       {text}
-    </Link >
+    </Link>
   </li>
 )
 
