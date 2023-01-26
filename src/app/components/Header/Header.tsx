796ed51d972a551503a99e9a7eb3adfbc97bfcd1
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-01-26 08:37:29 -0300

    fix: fix menu closing on desktop

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -6,18 +6,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { MouseEventHandler, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import menuData, { MenuItem } from './data'
-
-interface MenuItemLink extends MenuItem {
-  onClick: MouseEventHandler<HTMLAnchorElement>
-}
+import usePrevious from '@/app/hooks/usePrevious'
 
 const isActiveLink = (menuPath: string, routePath: string | null) =>
-  menuPath?.replace(/\/\d+/, '') === routePath?.replace(/\/\d+/, '')
+  menuPath.replace(/\/\d+/, '') === routePath?.replace(/\/\d+/, '')
 
-const MenuLink = ({ icon, href, onClick, text }: MenuItemLink) => {
+const MenuLink = ({ icon, href, text }: MenuItem) => {
   const currentPath = usePathname()
   const classes =
     'flex items-center px-5 py-3 rounded-md font-medium text-sm bg-[#212425] text-[#A6A6A6] hover:text-white hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476]'
@@ -27,7 +24,7 @@ const MenuLink = ({ icon, href, onClick, text }: MenuItemLink) => {
 
   return (
     <li className="px-2 xl:pl-5">
-      <Link className={className} href={href} onClick={onClick}>
+      <Link className={className} href={href}>
         <span className="mr-2 text-xl">
           <FontAwesomeIcon className="w-4" icon={icon} />
         </span>
@@ -40,7 +37,16 @@ const MenuLink = ({ icon, href, onClick, text }: MenuItemLink) => {
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false)
 
+  const currentPath = usePathname()
+  const previousPath = usePrevious(currentPath)
+
   const toggleMenuOpen = () => setMenuOpen(!menuOpen)
+
+  useEffect(() => {
+    if (currentPath !== previousPath && menuOpen) {
+      setMenuOpen(false)
+    }
+  }, [currentPath, previousPath, menuOpen])
 
   return (
     <header className="lg:container w-full h-24 flex items-center place-content-between bg-black lg:bg-transparent">
@@ -77,13 +83,7 @@ export default function Header() {
           }`}
         >
           {menuData.map(({ href, icon, text }, index) => (
-            <MenuLink
-              key={index}
-              icon={icon}
-              href={href}
-              text={text}
-              onClick={toggleMenuOpen}
-            />
+            <MenuLink key={index} icon={icon} href={href} text={text} />
           ))}
         </ul>
       </nav>
