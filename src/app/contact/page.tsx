8848b3bf156c65f03e96b5488e06d9ab29757569
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-01-27 18:08:58 -0300

    fix: fix margins on contact page for mobile

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,11 +6,11 @@ export default function About() {
     <div className="lg:container lg:rounded-2xl bg-[#111111] px-4 sm:px-5 md:px-10 lg:px-20">
       <div>
         <div className="sm:py-12 xs:pt-0 xs:pb-12">
-          <h2 className="after-effect after:left-40 mt-12 mb-10 xs:mt-6 lg:mt-0">
+          <h2 className="after-effect after:left-40 mt-12 mb-10 xs:mt-6 md:mt-0">
             Contact
           </h2>
           <div className="flex flex-col-reverse lg:flex-row">
-            <div className="flex flex-col space-y-6">
+            <div className="flex flex-col space-y-6 xs:mt-6 sm:mt-0">
               <ContactBadges />
             </div>
             <div className="flex flex-col grow items-center justify-center">
