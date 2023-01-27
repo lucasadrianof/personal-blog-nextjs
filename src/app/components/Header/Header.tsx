05a3a6d28b417ffeafc2b15fa408f720ea2beb19
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-01-27 17:56:21 -0300

    fix: color white on active link

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -20,10 +20,13 @@ const isActiveLink = (menuPath: string, routePath: string | null) =>
 
 const MenuLink = ({ icon, href, onClick, text }: MenuItemLink) => {
   const currentPath = usePathname()
+  const activeLink = isActiveLink(href, currentPath)
+
   const classes =
-    'flex items-center px-5 py-3 rounded-md font-medium text-sm bg-[#212425] text-[#A6A6A6] hover:text-white hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476]'
+    'flex items-center px-5 py-3 rounded-md font-medium text-sm bg-[#212425] hover:text-white hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476]'
   const className = cn(classes, {
-    ['bg-gradient-to-r text-white']: isActiveLink(href, currentPath),
+    'bg-gradient-to-r text-white': activeLink,
+    'text-[#A6A6A6]': !activeLink,
   })
 
   return (
