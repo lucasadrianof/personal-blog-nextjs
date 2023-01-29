45662582239b48149439e6f233cd7edd8d4f7717
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-01-29 00:05:54 -0300

    fix: close menu when clicking outside

---
+++ src/app/hooks/useClickOutside.tsx
@@ -0,0 +1,18 @@
+import { RefObject, useEffect, useState } from 'react'
+
+export default function useClickOutside(ref: RefObject<HTMLElement>) {
+  const [clickOutside, setClickOutside] = useState(false)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) =>
+      setClickOutside(!ref.current?.contains(event.target as HTMLElement))
+
+    document.addEventListener('click', handleClickOutside, true)
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true)
+    }
+  }, [ref])
+
+  return clickOutside
+}
