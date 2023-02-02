bd9e38372427d62129318f34b44014ca7b4883d5
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-02-02 12:06:17 -0300

    feat: cache sanity requests

--- src/lib/sanity/sanity.client.ts
+++ src/lib/sanity/sanity.client.ts
@@ -1,3 +1,6 @@
+import 'server-only'
+
+import { cache } from 'react'
 import { createClient } from 'next-sanity'
 import { postBySlugQuery, postsSlugsQuery } from './sanity.queries'
 
@@ -6,10 +9,13 @@ import { Post } from './types'
 
 const client = createClient({ apiVersion, dataset, projectId, useCdn })
 
-export const getAllPostsSlugs = async (): Promise<Pick<Post, 'slug'>[]> => {
-  const slugs = await client.fetch<string[]>(postsSlugsQuery)
-  return slugs.map((slug) => ({ slug }))
-}
+export const getAllPostsSlugs = cache(
+  async (): Promise<Pick<Post, 'slug'>[]> => {
+    const slugs = await client.fetch<string[]>(postsSlugsQuery)
+    return slugs.map((slug) => ({ slug }))
+  }
+)
 
-export const getPostBySlug = async (slug: string): Promise<Post> =>
-  client.fetch(postBySlugQuery, { slug })
+export const getPostBySlug = cache(
+  (slug: string): Promise<Post> => client.fetch(postBySlugQuery, { slug })
+)
