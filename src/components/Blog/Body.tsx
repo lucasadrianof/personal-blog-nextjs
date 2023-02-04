eb970b4dd75429e6fe9fdac454960c1ab3b2d1ac
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-02-04 10:44:36 -0300

    feat: custom header component for posts

--- src/components/Blog/Body.tsx
+++ src/components/Blog/Body.tsx
@@ -1,11 +1,15 @@
 import { PortableText, PortableTextComponents } from '@portabletext/react'
 
+import { H4Header } from '@/components/PortableText/Components/Header'
 import ImageComponent from '@/components/PortableText/Components/ImageComponent'
 import type { Post } from '@/lib/sanity/types'
 
 type BodyProps = Pick<Post, 'content'>
 
 const components: PortableTextComponents = {
+  block: {
+    h4: H4Header,
+  },
   types: {
     image: ImageComponent,
   },
