26dfcbe23d5c5b5fb90691c1769c3dd841b70cdd
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-02-07 08:18:08 -0300

    chore: remove tags from blog posts list

--- src/components/Blog/PostPreview.tsx
+++ src/components/Blog/PostPreview.tsx
@@ -1,18 +1,11 @@
 import Link from 'next/link'
 
 import Date from '@/components/Blog/Date'
-import Tags from '@/components/Blog/Tags'
 import { Post } from '@/lib/sanity/types'
 
-type PostPreviewProps = Pick<Post, 'date' | 'slug' | 'tags' | 'title'>
+type PostPreviewProps = Pick<Post, 'date' | 'slug' | 'title'>
 
-export default function PostPreview({
-  date,
-  slug,
-  tags,
-  title,
-}: PostPreviewProps) {
-  console.log({ tags })
+export default function PostPreview({ date, slug, title }: PostPreviewProps) {
   return (
     <Link
       className="bg-[#212425] text-white rounded-xl p-4"
@@ -21,7 +14,6 @@ export default function PostPreview({
       <div className="flex flex-col">
         <Date className="text-sm" dateString={date} />
         <span className="text-xl">{title}</span>
-        {tags && <Tags tags={tags} />}
       </div>
     </Link>
   )
