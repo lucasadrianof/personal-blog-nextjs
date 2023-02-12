676ee8a391f383ec7819d61609267dde2614e1d8
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-02-12 14:00:26 -0300

    fix: fix cache route

--- src/pages/api/cache-revalidate.ts
+++ src/pages/api/cache-revalidate.ts
@@ -1,32 +1,40 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { createClient, SanityClient } from "next-sanity";
+import { NextApiRequest, NextApiResponse } from 'next'
+import { createClient, SanityClient } from 'next-sanity'
 import { parseBody } from 'next-sanity/webhook'
 import type { SanityDocument, SlugValue } from 'sanity'
 
-import { apiVersion, dataset, projectId } from "@/lib/sanity/sanity.api";
+import { apiVersion, dataset, projectId } from '@/lib/sanity/sanity.api'
 import {
   nextPostByDateQuery,
   postByIdQuery,
   postSlugsByAuthorQuery,
-  previuousPostByDateQuery
-} from "@/lib/sanity/sanity.queries";
-import { Post } from "@/lib/sanity/types";
+  previuousPostByDateQuery,
+} from '@/lib/sanity/sanity.queries'
+import { Post } from '@/lib/sanity/types'
 
 const { SANITY_REVALIDATE_SECRET } = process.env
 
 type StaleRoute = '/blog' | `/blog/${string}`
 
-interface StaleRouteBody extends Pick<SanityDocument, '_type' | '_id' > {
+interface StaleRouteBody extends Pick<SanityDocument, '_type' | '_id'> {
   date?: string
   slug?: SlugValue
 }
 
-const getPostById = (client: SanityClient, id: string):Promise<Post> => client.fetch(postByIdQuery, { id })
-const getNextPost = (client: SanityClient, date: string):Promise<Post> => client.fetch(nextPostByDateQuery, { date })
-const getPreviousPost = (client: SanityClient, date: string):Promise<Post> => client.fetch(previuousPostByDateQuery, { date })
-const getPostSlugsByAuthor = (client: SanityClient, date: string):Promise<string[]> => client.fetch(postSlugsByAuthorQuery, { date })
+const getPostById = (client: SanityClient, id: string): Promise<Post> =>
+  client.fetch(postByIdQuery, { id })
+const getNextPost = (client: SanityClient, date: string): Promise<Post> =>
+  client.fetch(nextPostByDateQuery, { date })
+const getPreviousPost = (client: SanityClient, date: string): Promise<Post> =>
+  client.fetch(previuousPostByDateQuery, { date })
+const getPostSlugsByAuthor = (
+  client: SanityClient,
+  id: string
+): Promise<string[]> => client.fetch(postSlugsByAuthorQuery, { id })
 
-const queryStaleRoutes = async (body: StaleRouteBody): Promise<StaleRoute[]> => {
+const queryStaleRoutes = async (
+  body: StaleRouteBody
+): Promise<StaleRoute[]> => {
   const client = createClient({ apiVersion, dataset, projectId, useCdn: false })
   const { _id, _type } = body
 
@@ -35,13 +43,19 @@ const queryStaleRoutes = async (body: StaleRouteBody): Promise<StaleRoute[]> => 
     return await getStaleRoutesForDeletedPost(client, body)
 
   switch (_type) {
-  case 'post': return await queryStalePostRoutes(client, _id)
-  case 'author': return await queryStateAuthorRoutes(client, _id)
-  default: return []
+  case 'post':
+    return await queryStalePostRoutes(client, _id)
+  case 'author':
+    return await queryStateAuthorRoutes(client, _id)
+  default:
+    return []
   }
 }
 
-const getStaleRoutesForDeletedPost = async(client: SanityClient, body: StaleRouteBody): Promise<StaleRoute[]> => {
+const getStaleRoutesForDeletedPost = async (
+  client: SanityClient,
+  body: StaleRouteBody
+): Promise<StaleRoute[]> => {
   let staleRoutes: StaleRoute[] = ['/blog']
 
   const { date, slug } = body
@@ -55,43 +69,58 @@ const getStaleRoutesForDeletedPost = async(client: SanityClient, body: StaleRout
   return staleRoutes
 }
 
-const queryStalePostRoutes = async(client: SanityClient, id: string):Promise<StaleRoute[]> => {
+const queryStalePostRoutes = async (
+  client: SanityClient,
+  id: string
+): Promise<StaleRoute[]> => {
   const post = await getPostById(client, id)
   const nextPost = await getNextPost(client, post.date)
   const previousPost = await getPreviousPost(client, post.date)
   const stalePosts = [post, nextPost, previousPost]
-    .filter(post => !!post)
+    .filter((post) => !!post)
     .map(({ slug }) => `/blog/${slug}` as StaleRoute)
 
   return ['/blog', ...stalePosts]
 }
 
-const queryStateAuthorRoutes = async(client: SanityClient, id: string): Promise<StaleRoute[]> => {
+const queryStateAuthorRoutes = async (
+  client: SanityClient,
+  id: string
+): Promise<StaleRoute[]> => {
   const slugsByAuthor = await getPostSlugsByAuthor(client, id)
 
-  return ['/blog',
-    ...slugsByAuthor.map(slug => `/blog/${slug}` as StaleRoute),
+  return [
+    '/blog',
+    ...slugsByAuthor.map((slug) => `/blog/${slug}` as StaleRoute),
   ]
 }
 
-const queryNextPosts = async(client: SanityClient, date: string): Promise<StaleRoute[]> => {
+const queryNextPosts = async (
+  client: SanityClient,
+  date: string
+): Promise<StaleRoute[]> => {
   const nextPost = await getNextPost(client, date)
   let postSlugs: StaleRoute[] = []
 
   if (nextPost) {
     const nextPostSlugs = await queryNextPosts(client, nextPost.date)
-    postSlugs = [
-      `/blog/${nextPost.slug}` as StaleRoute,
-      ...nextPostSlugs,
-    ]
+    postSlugs = [`/blog/${nextPost.slug}` as StaleRoute, ...nextPostSlugs]
   }
 
   return postSlugs
 }
 
-export default async function cacheRevalidate(req: NextApiRequest, res: NextApiResponse) {
+export { config } from 'next-sanity/webhook'
+
+export default async function cacheRevalidate(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
-    const { body, isValidSignature } = await parseBody(req, SANITY_REVALIDATE_SECRET)
+    const { body, isValidSignature } = await parseBody(
+      req,
+      SANITY_REVALIDATE_SECRET
+    )
 
     if (!isValidSignature) {
       const message = 'Invalid signature'
@@ -105,15 +134,22 @@ export default async function cacheRevalidate(req: NextApiRequest, res: NextApiR
       return res.status(400).send(message)
     }
 
+    console.log({ body, raw: req.body })
     const staleRoutes = await queryStaleRoutes(body)
 
-    await Promise.all(staleRoutes.map(route => res.revalidate(route)))
+    console.log({ staleRoutes })
+
+    await Promise.all(staleRoutes.map((route) => res.revalidate(route)))
 
     const message = `Revalidated routes: ${staleRoutes.join(', ')}`
     console.log(message)
     return res.status(200).send(message)
   } catch (err) {
     console.error(err)
-    return res.status(500).send('An unexpected error occured, please reach out to hi@lucasfadriano.dev if you see this!')
+    return res
+      .status(500)
+      .send(
+        'An unexpected error occured, please reach out to hi@lucasfadriano.dev if you see this!'
+      )
   }
 }
