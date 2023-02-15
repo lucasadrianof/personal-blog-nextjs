4fdab95ade73ca002700cce2c75517c08569c02b
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-02-15 11:59:10 -0300

    Add comment section on blog posts (#51)
    * feat: basic commento integration
    
    * feat: render commento only when the env var is set
    
    * docs: add commento to README
    
    * chore: add rule to require eol-last
    
    * docs: added instructions on how to first sign-up to commento

---
+++ src/components/Commento/Commento.tsx
@@ -0,0 +1,17 @@
+import Script from "next/script";
+
+const { COMMENTO_URL } = process.env
+
+export default function Commento() {
+  return (
+    <>
+      {
+        COMMENTO_URL &&
+        <>
+          <Script src={`${process.env.COMMENTO_URL}/js/commento.js`} />
+          <div id="commento" />
+        </>
+      }
+    </>
+  )
+}
