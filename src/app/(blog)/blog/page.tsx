84c030ccc7730c04ce3a09f2ef8bf206533f26a4
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-02-16 08:53:12 -0300

    refactor: rename loop variables

--- src/app/(blog)/blog/page.tsx
+++ src/app/(blog)/blog/page.tsx
@@ -6,20 +6,20 @@ import { getAllPosts } from '@/lib/sanity/sanity.client'
 import type { Post } from '@/lib/sanity/types'
 
 type MonthYear = `${string}-${string}`
-type PostsByMonth = Record<MonthYear, Post[]>
+type PostsByMonthYear = Record<MonthYear, Post[]>
 
 export default async function Blog() {
   const allPosts = await getAllPosts()
-  const postsByMonth = groupPostsByMonth(allPosts)
+  const postsByMonthYear = groupPostsByMonth(allPosts)
 
   return (
     <div className="flex-grow">
       <div className="bg-gray-950 mx-auto px-5 lg:container lg:rounded-2xl md:px-10 lg:px-20">
         <div className="py-4">
           {
-            Object.entries(postsByMonth).map(([yearMonth, posts]) =>
-              <div key={yearMonth} className="mb-6">
-                <h3 className="border-b-2 inline-block pb-1 text-brand-slogan text-xl">{yearMonth}</h3>
+            Object.entries(postsByMonthYear).map(([monthYear, posts]) =>
+              <div key={monthYear} className="mb-6">
+                <h3 className="border-b-2 inline-block pb-1 text-brand-slogan text-xl">{monthYear}</h3>
                 <ul className="flex flex-col pt-4" >
                   {posts.map((post) =>
                     <li key={post.slug} className="flex items-center gap-x-8 mb-1">
@@ -38,7 +38,7 @@ export default async function Blog() {
 }
 
 const groupPostsByMonth = (posts: Post[]) =>
-  posts.reduce((acc:PostsByMonth, post) => {
+  posts.reduce((acc:PostsByMonthYear, post) => {
     const monthYear = format(parseISO(post.date), 'MM-yyyy') as MonthYear
 
     if (monthYear in acc) {
