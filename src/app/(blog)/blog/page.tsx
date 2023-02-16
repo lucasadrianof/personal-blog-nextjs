01bfdbe5034e58df64feceba24dc57f0fb9b2148
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-02-16 08:50:18 -0300

    feat: grouped posts by month/year

--- src/app/(blog)/blog/page.tsx
+++ src/app/(blog)/blog/page.tsx
@@ -1,20 +1,51 @@
+import { format, parseISO } from 'date-fns'
+
+import Date from '@/components/Blog/Date'
 import PostPreview from '@/components/Blog/PostPreview'
 import { getAllPosts } from '@/lib/sanity/sanity.client'
+import type { Post } from '@/lib/sanity/types'
+
+type MonthYear = `${string}-${string}`
+type PostsByMonth = Record<MonthYear, Post[]>
 
 export default async function Blog() {
   const allPosts = await getAllPosts()
+  const postsByMonth = groupPostsByMonth(allPosts)
 
   return (
     <div className="flex-grow">
       <div className="bg-gray-950 mx-auto px-5 lg:container lg:rounded-2xl md:px-10 lg:px-20">
         <div className="py-4">
-          <div className="flex flex-col gap-y-4">
-            {allPosts.map((post) =>
-              <PostPreview key={post.slug} {...post} />
-            )}
-          </div>
+          {
+            Object.entries(postsByMonth).map(([yearMonth, posts]) =>
+              <div key={yearMonth} className="mb-6">
+                <h3 className="border-b-2 inline-block pb-1 text-brand-slogan text-xl">{yearMonth}</h3>
+                <ul className="flex flex-col pt-4" >
+                  {posts.map((post) =>
+                    <li key={post.slug} className="flex items-center gap-x-8 mb-1">
+                      <Date className="hidden md:block text-sm" dateString={post.date} />
+                      <PostPreview {...post} />
+                    </li>
+                  )}
+                </ul>
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
   )
 }
+
+const groupPostsByMonth = (posts: Post[]) =>
+  posts.reduce((acc:PostsByMonth, post) => {
+    const monthYear = format(parseISO(post.date), 'MM-yyyy') as MonthYear
+
+    if (monthYear in acc) {
+      acc[monthYear].push(post)
+    } else {
+      acc[monthYear] = [post]
+    }
+
+    return acc
+  }, {})
--- src/components/Blog/PostPreview.tsx
+++ src/components/Blog/PostPreview.tsx
@@ -1,20 +1,13 @@
 import Link from 'next/link'
 
-import Date from '@/components/Blog/Date'
 import { Post } from '@/lib/sanity/types'
 
-type PostPreviewProps = Pick<Post, 'date' | 'slug' | 'title'>
+type PostPreviewProps = Pick<Post, 'slug' | 'title'>
 
-export default function PostPreview({ date, slug, title }: PostPreviewProps) {
+export default function PostPreview({ slug, title }: PostPreviewProps) {
   return (
-    <Link
-      className="bg-gray-850 text-white rounded-xl p-4"
-      href={`/blog/${slug}`}
-    >
-      <div className="flex flex-col">
-        <Date className="text-sm" dateString={date} />
-        <span className="text-xl">{title}</span>
-      </div>
+    <Link href={`/blog/${slug}`}>
+      <span className="text-xl text-brand-background-element underline">{title}</span>
     </Link>
   )
 }
