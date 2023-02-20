da31691a1ea5cb7b226549e9c673aedd581f5296
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2023-02-20 08:39:36 -0300

    feat: add animation to blog pages

--- src/app/(blog)/blog/page.tsx
+++ src/app/(blog)/blog/page.tsx
@@ -13,12 +13,20 @@ export default async function Blog() {
   const postsByMonthYear = groupPostsByMonth(allPosts)
 
   return (
-    <div className="flex-grow">
+    <div className="flex-grow" data-aos="fade" data-aos-duration={300}>
       <div className="bg-gray-950 mx-auto px-5 lg:container lg:rounded-2xl md:px-10 lg:px-20">
+        <div className="sm:py-12 xs:pt-0 xs:pb-12">
+          <h2 className="after-effect after:left-24 mt-12 xs:mt-6 lg:mt-0">
+            Blog
+          </h2>
+          <p className="pt-2 leading-7 text-gray-400 mt-2.5">
+            My personal blog, where I write about software development and other tech-related subjects.
+          </p>
+        </div>
         <div className="py-4">
           {
             Object.entries(postsByMonthYear).map(([monthYear, posts]) =>
-              <div key={monthYear} className="mb-6">
+              <div className="mb-6" key={monthYear}>
                 <h3 className="border-b-2 inline-block pb-1 text-brand-slogan text-xl">{monthYear}</h3>
                 <ul className="flex flex-col pt-4" >
                   {posts.map((post) =>
