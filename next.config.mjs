16afe6d4b60f41f38ab5347d3870bddc28f20f86
Lucas Felicio Adriano <lucas@lucasfadriano.dev>
2025-02-21 11:25:49 -0300

    feat: proxy posthog requests

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,25 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: 'cdn.sanity.io' }],
   },
+
+  async rewrites () {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://us.i.posthog.com/decide',
+      },
+    ]
+  },
+
+  skipTrailingSlashRedirect: true,
 }
 
 export default withBundleAnalyzer(nextConfig)
